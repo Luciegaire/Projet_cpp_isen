901ceaddc1a61b70544b3d4bc8990e6ab0c996dd
Thibaut C <t.chevallier11@gmail.com>
2020-05-15 15:38:24 +0200

    Modification expression.h

--- projet_cpp_isen/Expression.h
+++ projet_cpp_isen/Expression.h
@@ -13,6 +13,29 @@ public:
     virtual float calcul() = 0;
     virtual void affichageClassique() = 0;
     virtual void affichagePolonaiseInversee() = 0;
+	string GetFromFile(string filepath) 
+	{
+		string equation;
+		ifstream fileStream;
+		fileStream.open(filepath);
+		if (fileStream.is_open()) 
+		{
+			getline(fileStream, equation);
+			return equation;
+		}
+		else 
+		{
+			cout << "Le fichier est introuvable" << endl;
+			return "";
+		}
+	}
+	void SaveToFile(string toSave) 
+	{
+		ofstream savefile ("./save");
+		savefile << toSave;
+		savefile.close();
+		return;
+	}
 };
 
 #endif // EXPRESSION_H
